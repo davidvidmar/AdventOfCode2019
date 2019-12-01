2ab438c073a954e2da6715e7bdb7f4e1130fda41
David Vidmar <info@vidmar.net>
2019-12-01 22:24:38 +0100

    Day 1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,32 +1,72 @@
 ﻿using System;
-using System.IO;
 
-namespace Day01
+namespace AdventOfCode2019
 {
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Advent of Code 2019 - Day 1\n");
+            Utils.WriteStart(1);            
+            
+            Utils.WritePart(1);
 
-            // part 1
-            Console.WriteLine("\nPart 1");
+            // For a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
+            Console.WriteLine($"12 -> {CalculateFuel(12)} = 2");
+            // For a mass of 14, dividing by 3 and rounding down still yields 4, so the fuel required is also 2.
+            Console.WriteLine($"14 -> {CalculateFuel(14)} = 2");
+            // For a mass of 1969, the fuel required is 654.
+            Console.WriteLine($"1969 -> {CalculateFuel(1969)} = 654");
+            // For a mass of 100756, the fuel required is 33583
+            Console.WriteLine($"100756 -> {CalculateFuel(100756)} = 33583");
 
-            //foreach (var q in File.ReadAllLines("input.txt"))
-            //    Console.WriteLine(q);
+            var input = Utils.ReadAsInt("input.txt");
 
-            // ...
+            var sum = 0;
+            foreach (var i in input)
+            {
+                sum += CalculateFuel(i);
+            }
 
-            Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
+            Utils.WriteResult(sum);            
 
             // part 2
-            Console.WriteLine("\nPart 2");
+            Utils.WritePart(2);
 
-            // ...
+            Console.WriteLine($"14 -> {CalculateFuelRecursive(14)} = 2");
+            Console.WriteLine($"1969 -> {CalculateFuelRecursive(1969)} = 966");
+            Console.WriteLine($"100756 -> {CalculateFuelRecursive(100756)} = 50346");
 
+            var sum2 = 0;
+            foreach (var i in input)
+            {
+                sum2 += CalculateFuelRecursive(i);
+            }
+
+            Utils.WriteResult(sum2);
+
+            Console.WriteLine("Press any key to continue..."); 
+            Console.ReadKey();
         }
 
+        // Fuel required to launch a given module is based on its mass. 
+        // Specifically, to find the fuel required for a module, 
+        // take its mass, divide by three, round down, and subtract 2.
+        private static int CalculateFuel(int mass)
+        {
+            return (int)Math.Floor(mass / 3.0) - 2;
+        }
+
+        // For each module mass, calculate its fuel and add it to the total. 
+        // Then, treat the fuel amount you just calculated as the input mass and 
+        // repeat the process, continuing until a fuel requirement is zero or negative.
+        private static int CalculateFuelRecursive(int mass)
+        {            
+            var fuelMass = CalculateFuel(mass);
+            Console.WriteLine($" + {fuelMass}");
+            if (fuelMass > 0)
+                fuelMass += CalculateFuelRecursive(fuelMass);
+                
+            return fuelMass > 0 ? fuelMass : 0;
+        }
     }
-             
 }
---
+++ Utils/Utils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    public static class Utils
+    {
+        public static void WriteStart(int day)
+        {
+            Console.WriteLine($"Advent of Code 2019 - Day {day}");
+        }
+
+        public static void WriteResult(int value)
+        {
+            WriteResult(value.ToString());
+        }
+
+        public static void WriteResult(string value)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\nResult = {value}\n");
+            Console.ResetColor();
+        }
+
+        public static void WritePart(int part)
+        {
+            Console.ForegroundColor = ConsoleColor.White;            
+            Console.WriteLine($"\nPart {part}\n");
+            Console.ResetColor();
+        }
+
+        public static IEnumerable<Int32> ReadAsInt(string v)
+        {
+            return from q in File.ReadAllLines("input.txt") select Convert.ToInt32(q);
+        }
+    }
+}
