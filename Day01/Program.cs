302feb8662733153b8ef07bd5288356a47711614
David Vidmar <info@vidmar.net>
2019-12-01 20:27:10 +0100

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Advent of Code 2019 - Day 1\n");
+
+            // part 1
+            Console.WriteLine("\nPart 1");
+
+            //foreach (var q in File.ReadAllLines("input.txt"))
+            //    Console.WriteLine(q);
+
+            // ...
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            // part 2
+            Console.WriteLine("\nPart 2");
+
+            // ...
+
+        }
+
+    }
+             
+}
